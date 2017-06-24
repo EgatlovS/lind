c147e9869df50547c7f34bca1a7f5c27ca890ec7
EgatlovS <alex.bogner36@googlemail.com>
2017-06-24 16:14:08 +0200

    feat: added todos in bdrys

--- link-discovery-rs/src/main/java/de/egatlovs/linkdiscoveryrs/components/linkpoint/boundary/LinkpointBoundary.java
+++ link-discovery-rs/src/main/java/de/egatlovs/linkdiscoveryrs/components/linkpoint/boundary/LinkpointBoundary.java
@@ -6,26 +6,38 @@ import de.egatlovs.linkdiscoveryrs.components.linkpoint.entity.Linkpoint;
 public class LinkpointBoundary {
 
 	public Linkpoint getLinkpointById(long id) {
-		// TODO Auto-generated method stub
+		// TODO get endpoint from db
+		// TODO map endpoint to dto
+		// TODO return endpoint
 		return null;
 	}
 
 	public Linkpoint createLinkpoint(Linkpoint linkpoint) {
-		// TODO Auto-generated method stub
+		// TODO get structure of endpoint
+		// TODO validate against structure constraints
+		// TODO map linkpoint to entity
+		// TODO write linkpoint in db
+		// TODO return created linkpoint
 		return null;
 	}
 
 	public Linkpoint updateLinkpointById(long id, Linkpoint linkpoint) {
-		// TODO Auto-generated method stub
+		// TODO get linkpoint from db
+		// TODO map updated linkpoint to entity
+		// TODO get structure from db
+		// TODO validate updated linkpoint against structure
+		// TODO write updated linkpoint in db
+		// TODO return updated linkpoint
 		return null;
 	}
 
 	public void removeLinkpointById(long id) {
-		// TODO Auto-generated method stub
+		// TODO try to remove linkpoint from db
 	}
 
 	public Field getLinkpointField(long id, String fieldname) {
-		// TODO Auto-generated method stub
+		// TODO try to get field of linkpoint from db
+		// TODO return field
 		return null;
 	}
 
--- link-discovery-rs/src/main/java/de/egatlovs/linkdiscoveryrs/components/structure/boundary/StructureBoundary.java
+++ link-discovery-rs/src/main/java/de/egatlovs/linkdiscoveryrs/components/structure/boundary/StructureBoundary.java
@@ -8,40 +8,50 @@ import de.egatlovs.linkdiscoveryrs.components.structure.entity.Structure;
 public class StructureBoundary {
 
 	public List<Structure> getStructures() {
-		// TODO Auto-generated method stub
+		// TODO get structures from db
+		// TODO map structures to dtos
+		// TODO return structures
 		return null;
 	}
 
 	public Structure getStructureById(long id) {
-		// TODO Auto-generated method stub
+		// TODO get structure from db
+		// TODO map structure to dto
+		// TODO return dto
 		return null;
 	}
 
 	public Structure getStructureByName(String name) {
-		// TODO Auto-generated method stub
+		// TODO get structure from db
+		// TODO map structure to dto
+		// TODO return dto
 		return null;
 	}
 
 	public Structure createStructure(Structure structure) {
-		// TODO Auto-generated method stub
+		// TODO map structure to entity
+		// TODO write structure in db
+		// TODO return created structure
 		return null;
 	}
 
 	public void removeStructureById(long id) {
-		// TODO Auto-generated method stub
+		// TODO try to remove structure and attached linkpoints from db
 	}
 
 	public void removeStructureByName(String name) {
-		// TODO Auto-generated method stub
+		// TODO try to remove structure and attached linkpoints from db
 	}
 
 	public List<Linkpoint> getLinkpointsByStructureId(long id) {
-		// TODO Auto-generated method stub
+		// TODO get linkpoints from db
+		// TODO map linkpoints to dto
+		// TODO return linkpoints
 		return null;
 	}
 
 	public void removeLinkpointsByStructureId(long id) {
-		// TODO Auto-generated method stub
+		// TODO try to remove all linkpoints from that sturcture from db
 	}
 
 }
