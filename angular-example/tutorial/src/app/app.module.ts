80870fa4afe5dfbfba4d2493d4e130f704c3a6d5
Linus <linussee@web.de>
2017-06-29 19:50:42 +0200

    feat: initial commit

--- angular-example/tutorial/src/app/app.module.ts
+++ angular-example/tutorial/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { TopicPanelComponent } from './tutorial/topic-panel/topic-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TutorialComponent,
+    TopicPanelComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-example/tutorial/src/app/tutorial/topic-panel/topic-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lt-topic-panel',
+  templateUrl: './topic-panel.component.html',
+  styleUrls: ['./topic-panel.component.css']
+})
+export class TopicPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
