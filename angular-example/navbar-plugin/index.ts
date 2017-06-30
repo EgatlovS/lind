81cf2b72b7f4a29b1b3fd2e812b1c5f3a079c33c
Linus <linussee@web.de>
2017-06-30 22:47:39 +0200

    feat: initial commit

---
+++ angular-example/navbar-plugin/index.ts
@@ -0,0 +1,1 @@
+export { LindNavbarModule } from './lind-navbar.module';
---
+++ angular-example/navbar-plugin/src/components/navbar/lind-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'lind-navbar',
+   templateUrl: 'lind-navbar.component.html',
+   styleUrls: ['lind-navbar.component.css']
+})
+export class LindNavbarComponent
+{
+
+}
