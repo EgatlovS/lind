3afd6a1136a3abf124499a43909b328f92c8abcc
Linus <linussee@web.de>
2017-06-30 23:50:00 +0200

    feat: Adding the current navbar directly from node_modules

---
+++ angular-example/tutorial/node_modules/navbar-plugin/lind-navbar.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { LindNavbarComponent } from './src/components/navbar/lind-navbar.component';
+
+@NgModule({
+   declarations: [
+      LindNavbarComponent
+   ],
+   exports: [
+      LindNavbarComponent
+   ]
+})
+export class LindNavbarModule
+{
+
+}
