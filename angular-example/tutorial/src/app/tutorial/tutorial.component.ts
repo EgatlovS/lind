210d93539504e8a1fef3b06eabf36060e64af4b9
Linus <linussee@web.de>
2017-06-30 23:02:35 +0200

    feat: creates a list of topic-panel.component

--- angular-example/tutorial/src/app/tutorial/tutorial.component.ts
+++ angular-example/tutorial/src/app/tutorial/tutorial.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+/*
+ * Displays a list of panels that each resemble a tutorial
+ * A panel is represented by the 'topic-panel.component'
+ */
 @Component({
   selector: 'lt-tutorial',
   templateUrl: './tutorial.component.html',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialComponent implements OnInit {
 
+  // Tutorials displayed
+  values: { title: string, content: string }[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    // TODO: Init values by getting the structure/linkpoint from the server
+    this.values.push({title: 'How to navbar', content: 'Tutorial Anleitung'});
+    this.values.push({title: 'How to Dynamic Components', content: 'Tutorial'});
   }
 
 }
