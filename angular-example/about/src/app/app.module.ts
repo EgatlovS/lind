0e65615dfd055f21a1515f06575386d162b9e704
Linus <linussee@web.de>
2017-07-03 18:51:14 +0200

    feat: Added angular2-markdown to the project

--- angular-example/about/src/app/app.module.ts
+++ angular-example/about/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MarkdownModule } from 'angular2-markdown';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
