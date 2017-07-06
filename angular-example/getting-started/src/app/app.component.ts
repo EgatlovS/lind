4fa9b2a9c138256dc487cb41ec1e06f1dedf3ffc
Linus <linussee@web.de>
2017-07-06 22:07:37 +0200

    refactor: filling the panel components data with files from the server

--- angular-example/getting-started/src/app/app.component.ts
+++ angular-example/getting-started/src/app/app.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FileService } from './core/services/file/file.service';
+import { PanelContent } from './models/PanelContent';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-   structureId = 1;
-   fieldNames = {tabname: 'TabName', applicationLink: 'ApplicationLink'};
+export class AppComponent implements OnInit {
+  structureId = 1;
+  fieldNames = { tabname: 'TabName', applicationLink: 'ApplicationLink' };
+
+  content = {
+               setupLind: new PanelContent(),
+               structCreation: new PanelContent(),
+               linkCreation: new PanelContent(),
+               setupAngular: new PanelContent()
+            };
+
+  constructor(private fileService: FileService) { }
+
+  ngOnInit(): void {
+     this.content.setupLind.name = 'Setup lind-rs';
+     this.content.structCreation.name = 'Structure Creation';
+     this.content.linkCreation.name = 'Link Creation';
+     this.content.setupAngular.name = 'Setup Angular Example';
+
+     this.fileService.asPlainText('assets/panel-content/setup-lind.txt').subscribe(
+       data => this.content.setupLind.value = data,
+       err => console.log('Err:', err)
+     );
+     this.fileService.asPlainText('assets/panel-content/structure-creation.txt').subscribe(
+       data => this.content.structCreation.value = data,
+       err => console.log('Err:', err)
+     );
+     this.fileService.asPlainText('assets/panel-content/link-creation.txt').subscribe(
+       data => this.content.linkCreation.value = data,
+       err => console.log('Err:', err)
+     );
+     this.fileService.asPlainText('assets/panel-content/setup-angular.txt').subscribe(
+        data => this.content.setupAngular.value = data,
+        err => console.log('Err:', err)
+     );
+  }
 }
