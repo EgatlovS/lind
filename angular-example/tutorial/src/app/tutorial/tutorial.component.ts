40a07fb40b656550c6d0e9c158e5a7865ecb2a41
Linus <linussee@web.de>
2017-07-07 22:40:52 +0200

    feat: get 'how to navbar' content from server

--- angular-example/tutorial/src/app/tutorial/tutorial.component.ts
+++ angular-example/tutorial/src/app/tutorial/tutorial.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FileService } from './../core/services/file/file.service';
+
 
 /*
  * Displays a list of panels that each resemble a tutorial
@@ -15,11 +17,16 @@ export class TutorialComponent implements OnInit {
   // Tutorials displayed
   values: { title: string, content: string }[] = [];
 
-  constructor() { }
+  constructor(private fileService: FileService) { }
 
   ngOnInit() {
     // TODO: Init values by getting the structure/linkpoint from the server
-    this.values.push({title: 'How to navbar', content: 'Tutorial Anleitung'});
-    this.values.push({title: 'How to Dynamic Components', content: 'Tutorial'});
+    this.values.push({title: 'How to navbar', content: undefined});
+    this.values.push({title: 'How to Dynamic Components', content: undefined});
+
+    this.fileService.asPlainText('assets/tutorials/how-to-navbar.txt').subscribe(
+      data => this.values[0].content = data,
+      err => console.log('Err', err)
+   );
   }
 }
